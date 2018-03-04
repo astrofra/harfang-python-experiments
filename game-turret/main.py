5595521de155d3967abd2915bddd5085f1de1dd0
astrofra <astrofra@gmail.com>
2018-03-04 13:43:23 +0100

    Updated the source to comply with the new HARFANG API (pre-V1.0).

--- game-turret/main.py
+++ game-turret/main.py
@@ -1,14 +1,14 @@
-import gs
+import harfang as hg
 from os import path
 from math import radians, cos, sin
 from random import uniform, randint
 from constants import *
 
-gs.LoadPlugins()
+hg.LoadPlugins()
 
 
 def rvect(r):
-	return gs.Vector3(uniform(-r, r), uniform(-r, r), uniform(-r, r))
+	return hg.Vector3(uniform(-r, r), uniform(-r, r), uniform(-r, r))
 
 
 def setup_game_level(plus=None):
@@ -19,25 +19,25 @@ def setup_game_level(plus=None):
 
 	scn.GetPhysicSystem().SetDebugVisuals(False)
 
-	cam_matrix = gs.Matrix4.TransformationMatrix(gs.Vector3(0, 15, 3), gs.Vector3(radians(90), 0, 0))
+	cam_matrix = hg.Matrix4.TransformationMatrix(hg.Vector3(0, 15, 3), hg.Vector3(radians(90), 0, 0))
 	plus.AddCamera(scn, cam_matrix)
-	plus.AddLight(scn, gs.Matrix4.TransformationMatrix(gs.Vector3(-10, 10, 5), gs.Vector3(radians(55), radians(100), 0)), gs.Light.Model_Spot, 40.0, True)
-	env_blue = gs.Color.Blue + gs.Color.Green * 0.5
-	plus.AddLight(scn, gs.Matrix4.TranslationMatrix(gs.Vector3(10, -25, -5)), gs.Light.Model_Point, 0.0, False, env_blue, gs.Color.Black)
-	plus.AddEnvironment(scn, gs.Color.Black, env_blue * 0.15)
+	plus.AddLight(scn, hg.Matrix4.TransformationMatrix(hg.Vector3(-10, 10, 5), hg.Vector3(radians(55), radians(100), 0)), hg.LightModelSpot, 40.0, True)
+	env_blue = hg.Color.Blue + hg.Color.Green * 0.5
+	plus.AddLight(scn, hg.Matrix4.TranslationMatrix(hg.Vector3(10, -25, -5)), hg.LightModelPoint, 0.0, False, env_blue, hg.Color.Black)
+	plus.AddEnvironment(scn, hg.Color.Black, env_blue * 0.15)
 	ground = plus.AddPhysicPlane(scn)
 
 	return scn, ground
 
 
-def create_turret(plus=None, scn=None, pos=gs.Vector3(0, 0.75, 0), rot=gs.Vector3(), w=1, h=1.25, d=1, mass = 10):
-	scn.GetPhysicSystem().SetForceRigidBodyAxisLockOnCreation(gs.LockX + gs.LockY + gs.LockZ + gs.LockRotX + gs.LockRotZ)
-	root = plus.AddPhysicCube(scn, gs.Matrix4.TransformationMatrix(pos, rot), w, h, d, mass)
+def create_turret(plus=None, scn=None, pos=hg.Vector3(0, 0.75, 0), rot=hg.Vector3(), w=1, h=1.25, d=1, mass = 10):
+	scn.GetPhysicSystem().SetForceRigidBodyAxisLockOnCreation(hg.AxisLockX + hg.AxisLockY + hg.AxisLockZ + hg.AxisLockRotX + hg.AxisLockRotZ)
+	root = plus.AddPhysicCube(scn, hg.Matrix4.TransformationMatrix(pos, rot), w, h, d, mass)
 	root[0].SetName('turret')
 	root[1].SetAngularDamping(1.0)
 	cannon_geo = plus.CreateGeometry(plus.CreateCylinder(w * 0.25, d))
-	cannon = plus.AddObject(scn, cannon_geo, gs.Matrix4.TransformationMatrix(gs.Vector3(0, h * 0.2, d * 0.75),
-																			 gs.Vector3(radians(90), 0, 0)))
+	cannon = plus.AddObject(scn, cannon_geo, hg.Matrix4.TransformationMatrix(hg.Vector3(0, h * 0.2, d * 0.75),
+																			 hg.Vector3(radians(90), 0, 0)))
 	cannon.GetTransform().SetParent(root[0])
 
 	return root, cannon, mass
@@ -47,20 +47,20 @@ def rotate_turret(turret, angle, mass):
 	rot = turret[0].GetTransform().GetRotation()
 	dt_rot = radians(angle) - rot.y
 	turret[1].SetIsSleeping(False)
-	turret[1].ApplyTorque(gs.Vector3(0, dt_rot * mass * turret_rotation_speed, 0))
+	turret[1].ApplyTorque(hg.Vector3(0, dt_rot * mass * turret_rotation_speed, 0))
 
 
-def spawn_enemy(plus, scn, pos = gs.Vector3(0, 2, 5)):
+def spawn_enemy(plus, scn, pos = hg.Vector3(0, 2, 5)):
 	scn.GetPhysicSystem().SetForceRigidBodyAxisLockOnCreation(0)
-	root = plus.AddPhysicSphere(scn, gs.Matrix4.TranslationMatrix(pos), 0.7, 6, 16, enemy_mass, "assets/materials/green.mat")
+	root = plus.AddPhysicSphere(scn, hg.Matrix4.TranslationMatrix(pos), 0.7, 6, 16, enemy_mass, "assets/materials/green.mat")
 	root[0].SetName('enemy')
 
 	return root
 
 
 def throw_bullet(plus, scn, pos, dir):
-	scn.GetPhysicSystem().SetForceRigidBodyAxisLockOnCreation(gs.LockY)
-	root = plus.AddPhysicSphere(scn, gs.Matrix4.TranslationMatrix(pos), 0.2, 3, 8, 1.0, "assets/materials/grey.mat")
+	scn.GetPhysicSystem().SetForceRigidBodyAxisLockOnCreation(hg.AxisLockY)
+	root = plus.AddPhysicSphere(scn, hg.Matrix4.TranslationMatrix(pos), 0.2, 3, 8, 1.0, "assets/materials/grey.mat")
 	root[0].SetName('bullet')
 	root[1].ApplyLinearImpulse(dir * bullet_velocity)
 
@@ -74,13 +74,13 @@ def destroy_enemy(plus, scn, enemy):
 def render_aim_cursor(plus, scn, angle):
 	radius = screen_width / 8
 	angle = 90 - angle
-	a = gs.Vector2(cos(radians(angle)), sin(radians(angle))) * radius * 1.15
-	b = gs.Vector2(cos(radians(angle - 5)), sin(radians(angle - 5))) * radius
-	c = gs.Vector2(cos(radians(angle + 5)), sin(radians(angle + 5))) * radius
+	a = hg.Vector2(cos(radians(angle)), sin(radians(angle))) * radius * 1.15
+	b = hg.Vector2(cos(radians(angle - 5)), sin(radians(angle - 5))) * radius
+	c = hg.Vector2(cos(radians(angle + 5)), sin(radians(angle + 5))) * radius
 	plus.Triangle2D(screen_width * 0.5 + a.x, screen_height * 0.15 + a.y,
 					screen_width * 0.5 + b.x, screen_height * 0.15 + b.y,
 					screen_width * 0.5 + c.x, screen_height * 0.15 + c.y,
-					gs.Color.Green, gs.Color.Green, gs.Color.Green)
+					hg.Color.Green, hg.Color.Green, hg.Color.Green)
 
 
 def display_hud(plus, player_energy, cool_down, score):
@@ -90,19 +90,19 @@ def display_hud(plus, player_energy, cool_down, score):
 				player_energy * screen_width * 0.15, screen_height * 0.225,
 				player_energy * screen_width * 0.15, screen_height * 0.175,
 				screen_width * 0.015, screen_height * 0.175,
-				gs.Color.Green, gs.Color.Green, gs.Color.Green, gs.Color.Green)
-	plus.Text2D(screen_width * 0.018, screen_height * 0.1825, "LIFE", font_size, gs.Color.White, screen_font)
+				hg.Color.Green, hg.Color.Green, hg.Color.Green, hg.Color.Green)
+	plus.Text2D(screen_width * 0.018, screen_height * 0.1825, "LIFE", font_size, hg.Color.White, screen_font)
 
 	plus.Quad2D(screen_width * 0.015, screen_height * 0.15,
 				cool_down * screen_width * 0.15, screen_height * 0.15,
 				cool_down * screen_width * 0.15, screen_height * 0.1,
 				screen_width * 0.015, screen_height * 0.1,
-				gs.Color.Green, gs.Color.Green, gs.Color.Green, gs.Color.Green)
+				hg.Color.Green, hg.Color.Green, hg.Color.Green, hg.Color.Green)
 
-	plus.Text2D(screen_width * 0.018, screen_height * 0.1075, "HEAT", font_size, gs.Color.White, screen_font)
+	plus.Text2D(screen_width * 0.018, screen_height * 0.1075, "HEAT", font_size, hg.Color.White, screen_font)
 
-	plus.Text2D(screen_width * 0.018, screen_height * 0.035, "SCORE", font_size, gs.Color.White, screen_font)
-	plus.Text2D(screen_width * 0.15, screen_height * 0.035, str(score), font_size, gs.Color.Green, screen_font)
+	plus.Text2D(screen_width * 0.018, screen_height * 0.035, "SCORE", font_size, hg.Color.White, screen_font)
+	plus.Text2D(screen_width * 0.15, screen_height * 0.035, str(score), font_size, hg.Color.Green, screen_font)
 
 
 def create_explosion(plus, scn, pos, debris_amount=32, debris_radius=0.5):
@@ -110,7 +110,7 @@ def create_explosion(plus, scn, pos, debris_amount=32, debris_radius=0.5):
 	new_debris_list = []
 	for i in range(debris_amount):
 		debris_size = uniform(0.1, 0.25)
-		debris = plus.AddPhysicCube(scn, gs.Matrix4().TransformationMatrix(pos + rvect(debris_radius), rvect(radians(45))),
+		debris = plus.AddPhysicCube(scn, hg.Matrix4().TransformationMatrix(pos + rvect(debris_radius), rvect(radians(45))),
 									debris_size, debris_size, debris_size, 0.05, "assets/materials/green.mat")
 		debris[1].ApplyLinearImpulse(rvect(0.25))
 		new_debris_list.append(debris[0])
@@ -126,39 +126,40 @@ def play_sound_fx(mixer, sound_type):
 
 
 def display_title_screen(plus, scn):
-	plus.Text2D(screen_width * 0.15, screen_height * 0.625, "BOULDER\nATTACKS", font_size * 4.25, gs.Color(0,0,0,0.25), screen_font)
-	plus.Text2D(screen_width * 0.15, screen_height * 0.65, "BOULDER\nATTACKS", font_size * 4.25, gs.Color.Green, screen_font)
+	plus.Text2D(screen_width * 0.15, screen_height * 0.625, "BOULDER\nATTACKS", font_size * 4.25, hg.Color(0,0,0,0.25), screen_font)
+	plus.Text2D(screen_width * 0.15, screen_height * 0.65, "BOULDER\nATTACKS", font_size * 4.25, hg.Color.Green, screen_font)
 
-	fade = abs(sin(plus.GetClock().to_sec()))
+	fade = abs(sin(hg.time_to_sec_f(plus.GetClock())))
 	plus.Text2D(screen_width * 0.35, screen_height * 0.35, "PRESS SPACE", font_size * 1.25,
-				gs.Color.Green * gs.Color(1, 1, 1, fade), screen_font)
+				hg.Color.Green * hg.Color(1, 1, 1, fade), screen_font)
 
 
 def display_game_over(plus, scn, score):
-	plus.Text2D(screen_width * 0.2, screen_height * 0.625, "GAME\n  OVER", font_size * 4.25, gs.Color(0,0,0,0.25), screen_font)
-	plus.Text2D(screen_width * 0.2, screen_height * 0.65, "GAME\n  OVER", font_size * 4.25, gs.Color.Red, screen_font)
+	plus.Text2D(screen_width * 0.2, screen_height * 0.625, "GAME\n  OVER", font_size * 4.25, hg.Color(0,0,0,0.25), screen_font)
+	plus.Text2D(screen_width * 0.2, screen_height * 0.65, "GAME\n  OVER", font_size * 4.25, hg.Color.Red, screen_font)
 
 	plus.Text2D(screen_width * 0.3, screen_height * 0.35, "YOU SCORED " + str(score), font_size * 1.25,
-				gs.Color.Red, screen_font)
+				hg.Color.Red, screen_font)
 
-	fade = abs(sin(plus.GetClock().to_sec()))
+	fade = abs(sin(hg.time_to_sec_f(plus.GetClock())))
 	plus.Text2D(screen_width * 0.3, screen_height * 0.25, "PRESS SPACE", font_size * 1.25,
-				gs.Color.Red * gs.Color(1, 1, 1, fade), screen_font)
+				hg.Color.Red * hg.Color(1, 1, 1, fade), screen_font)
 
 
 def game():
-	plus = gs.GetPlus()
+	plus = hg.GetPlus()
 	plus.RenderInit(screen_width, screen_height)
-	al = gs.ALMixer()
+	al = hg.CreateMixer()
 	al.Open()
-	gs.MountFileDriver(gs.StdFileDriver())
+	hg.MountFileDriver(hg.StdFileDriver())
+	keyboard = hg.GetInputSystem().GetDevice("keyboard")
 
 	scn, ground = setup_game_level(plus)
 	turret, cannon, turret_mass = create_turret(plus, scn)
 
 	game_state = "GAME_INIT"
 
-	while not plus.KeyPress(gs.InputDevice.KeyEscape):
+	while not plus.IsAppEnded():
 		dt = plus.UpdateClock()
 
 		# Initialize Game
@@ -177,37 +178,37 @@ def game():
 		# Title screen
 		if game_state == "TITLE":
 			display_title_screen(plus, scn)
-			if plus.KeyReleased(gs.InputDevice.KeySpace):
+			if plus.KeyReleased(hg.KeySpace):
 				game_state = "GAME"
 		# Game
 		elif game_state == "GAME":
 			# Turret
-			if plus.KeyDown(gs.InputDevice.KeyRight):
-				target_angle += dt.to_sec() * aim_rotation_speed
+			if plus.KeyDown(hg.KeyRight):
+				target_angle += hg.time_to_sec_f(dt) * aim_rotation_speed
 			else:
-				if plus.KeyDown(gs.InputDevice.KeyLeft):
-					target_angle -= dt.to_sec() * aim_rotation_speed
+				if plus.KeyDown(hg.KeyLeft):
+					target_angle -= hg.time_to_sec_f(dt) * aim_rotation_speed
 
-			if plus.KeyPress(gs.InputDevice.KeySpace):
+			if plus.KeyPress(hg.KeySpace):
 				if turret_cool_down < 0.0:
 					throw_bullet(plus, scn, cannon.GetTransform().GetWorld().GetTranslation(), cannon.GetTransform().GetWorld().GetRow(1))
 					turret_cool_down = turret_cool_down_duration
 					play_sound_fx(al, 'shoot')
 				else:
 					play_sound_fx(al, 'error')
-					turret_cool_down += 10.0 * dt.to_sec()
+					turret_cool_down += 10.0 * hg.time_to_sec_f(dt)
 
-			turret_cool_down -= dt.to_sec()
+			turret_cool_down -= hg.time_to_sec_f(dt)
 
 			target_angle = max(min(target_angle, aim_angle_range['max']), aim_angle_range['min'])
 
 			rotate_turret(turret, target_angle, turret_mass)
 
 			# Enemies
-			spawn_timer += dt.to_sec()
+			spawn_timer += hg.time_to_sec_f(dt)
 			if spawn_timer > enemy_spawn_interval:
 				spawn_timer = 0
-				spawn_pos = gs.Vector3(uniform(-10, 10), 2.5, uniform(5.5, 6.5))
+				spawn_pos = hg.Vector3(uniform(-10, 10), 2.5, uniform(5.5, 6.5))
 				spawn_pos.Normalize()
 				spawn_pos *= 10.0
 				spawn_pos.y = 5.0
@@ -248,13 +249,13 @@ def game():
 							score += 10
 
 				# Game difficulty
-				enemy_spawn_interval = max(1.0, enemy_spawn_interval - dt.to_sec() * 0.025)
+				enemy_spawn_interval = max(1.0, enemy_spawn_interval - hg.time_to_sec_f(dt) * 0.025)
 
 				# Cleanup debris
 				if len(debris_list) > max_debris:
 					tmp_debris = debris_list[0]
 					debris_list.remove(debris_list[0])
-					tmp_debris.RemoveComponent(tmp_debris.GetComponent("RigidBody"))
+					tmp_debris.RemoveComponent(tmp_debris.GetRigidBody())
 					# scn.RemoveNode(tmp_debris)
 
 			render_aim_cursor(plus, scn, target_angle)
@@ -263,7 +264,7 @@ def game():
 		# Game over screen
 		elif game_state == "GAME_OVER":
 			display_game_over(plus, scn, score)
-			if plus.KeyReleased(gs.InputDevice.KeySpace):
+			if plus.KeyReleased(hg.KeySpace):
 				game_state = "SCENE_RESET"
 
 		# Reset the playfield for a new game
@@ -272,11 +273,12 @@ def game():
 				destroy_enemy(plus, scn, enemy[0])
 
 			for debris in debris_list:
-				debris.RemoveComponent(debris.GetComponent("RigidBody"))
+				debris.RemoveComponent(debris.GetRigidBody())
 
 			game_state = "GAME_INIT"
 
 		plus.UpdateScene(scn, dt)
 		plus.Flip()
+		plus.EndFrame()
 
 game()
